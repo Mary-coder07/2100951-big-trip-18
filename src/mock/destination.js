66b471a88abe9e409b41449e0e8fe6ac074d3d95
PsinaM <zpupka040@gmail.com>
2022-08-23 20:02:09 +0300

    module2-task1

---
+++ src/mock/destination.js
@@ -0,0 +1,27 @@
+import { CITY_DESC, CITIES } from './consts.js';
+import { getRandomInteger, getRandomElementsFromArray } from '../view/utils.js';
+
+const generateDestination = (id) => ({
+    id,
+    description: getRandomElementsFromArray(CITY_DESC),
+    name: CITIES[getRandomInteger(0, CITIES.length - 1)],
+    pictures: [
+        {
+            src: `http://picsum.photos/248/152?r=${getRandomInteger(0, 100)}`,
+            description: CITY_DESC[getRandomInteger(0, CITY_DESC.length - 1)].slice(getRandomInteger(0, CITY_DESC.length - 1)),
+        }
+    ]
+});
+
+const getDestinations = (count) => {
+    const dests = [];
+    for (let i = 0; i < count; i++) {
+        dests.push(generateDestination(i));
+    }
+    return dests;
+};
+
+const destinations = getDestinations(CITIES.length);
+
+
+export { destinations };
---
+++ src/model/points-model.js
@@ -0,0 +1,8 @@
+import { generatePoint } from '../mock/point.js';
+import { SUM_POINTS } from '../mock/consts.js';
+
+export default class PointsModel {
+  points = Array.from({ length: SUM_POINTS }, generatePoint);
+
+  getPoints = () => this.points;
+}
