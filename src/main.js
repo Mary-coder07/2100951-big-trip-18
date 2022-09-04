76a676a01664fbec15f4f8ee142ddd40a43858dc
Мария Некрасова <zpupka040@gmail.com>
2022-09-05 00:35:57 +0300

    module4-task2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { render } from './framework/render.js';
 import FilterView from './view/filter-view.js';
 import EventsPresenter from './presenter/events-presenter.js';
 import PointsModel from './model/points-model.js';
+import { generateFilter } from './mock/filter.js';
 
 const pageHeaderElement = document.querySelector('.page-header');
 const filtersElement = pageHeaderElement.querySelector('.trip-controls__filters');
@@ -10,7 +11,8 @@ const tripEventsElement = pageMainElement.querySelector('.trip-events');
 
 const eventsPresenter = new EventsPresenter();
 const pointsModel = new PointsModel();
+const filters = generateFilter(pointsModel.tasks);
 
-render(new FilterView(), filtersElement);
+render(new FilterView(filters), filtersElement);
 
 eventsPresenter.init(tripEventsElement, pointsModel);
