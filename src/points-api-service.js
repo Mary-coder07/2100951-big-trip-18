6d836f03f41e61e20f1db4233780d3b12d46fb3a
Мария Некрасова <mary.liss133@gmail.com>
2022-10-08 19:45:52 +0300

    fix

--- src/points-api-service.js
+++ src/points-api-service.js
@@ -43,12 +43,10 @@ export default class PointApiService extends ApiService {
     return parsedResponse;
   };
 
-  deletePoint = async (point) => {
-    const response = await this._load({
-      url: `points/${point.id}`,
-      method: Method.DELETE,
-    });
-  }
+  deletePoint = async (point) => await this._load({
+    url: `points/${point.id}`,
+    method: Method.DELETE,
+  });
 
   #adaptToServer = (point) => {
     const adaptedPoint = {
