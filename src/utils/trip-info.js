78177c1c106f7578eec753bde7c1c139d1be25bc
Мария Некрасова <mary.liss133@gmail.com>
2022-10-08 20:01:05 +0300

    fix

--- src/utils/trip-info.js
+++ src/utils/trip-info.js
@@ -4,7 +4,7 @@ const getTripInfo = (pointsModel) => {
   const pointsSequence = pointsModel.points.slice(0);
   let tripCost = 0;
   pointsSequence.sort((a, b) => dayjs(a.dateFrom).isAfter(b.dateFrom) ? 1 : -1)
-   .forEach((point) => {
+    .forEach((point) => {
       const offersByType = pointsModel.offers.find((offer) => offer.type === point.type).offers;
       offersByType.forEach((offerByType) => {
         if (point.offers.includes(offerByType.id)) {
